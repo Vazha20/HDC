c9f5ee130ed47b46e887969e58ba0ada3e076e1e
Vazha20 <gabisoniavazha@gmail.com>
2024-01-21 19:01:06 +0400

    newnew

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const burgerMenuBtn = document.querySelector('.burger-menu-btn');
-        const burgerSidebar = document.querySelector('.burger-sidebar');
-
-        burgerMenuBtn.addEventListener('click', function () {
-            burgerSidebar.style.display = (burgerSidebar.style.display === 'block') ? 'none' : 'block';
-        });
-    });
-
